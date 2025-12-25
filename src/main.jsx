b1948d9a24399bab85aa3396fde04aa0f5fa3ae0
thagreatjoel <joeljonah06@gmail.com>
2025-12-25 19:14:07 +0530

    Fixed some fishy bugs

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react_dom/client'
+import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'theme-ui'
 import theme from '@hackclub/theme'
 import App from './App'
 
-ReactDOM .createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <ThemeProvider theme={theme} colorMode="dark">
-  <App/>
+    <App />
   </ThemeProvider>
 )
